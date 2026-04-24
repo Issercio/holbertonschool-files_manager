9d5e8f427233e6c69ce181219067aecd19d0512e
Issercio <didi_1374@outlook.fr>
2026-04-24 09:17:27 +0200

    add the project

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,6 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
-class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body || {};
     if (!email) {
@@ -17,6 +16,23 @@ class UsersController {
     const hashedPassword = sha1(password);
     const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
     return res.status(201).json({ id: result.insertedId.toString(), email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const key = `auth_${token}`;
+    const userId = await (await import('../utils/redis')).default.get(key);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const user = await dbClient.db.collection('users').findOne({ _id: (await import('mongodb')).ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    return res.status(200).json({ id: user._id.toString(), email: user.email });
   }
 }
 
